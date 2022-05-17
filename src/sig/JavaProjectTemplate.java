18a055ee8937bcb987e2184df316944d31226e1c
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-05-17 19:39:23 +0000

    Bring back java files

--- src/sig/JavaProjectTemplate.java
+++ src/sig/JavaProjectTemplate.java
@@ -1,1 +1,21 @@
-404: Not Found
+package sig;
+
+import javax.swing.JFrame;
+import sig.engine.Panel;
+
+public class JavaProjectTemplate {
+	public static final String PROGRAM_NAME="Sig's Java Project Template";
+	public static void main(String[] args) {
+		JFrame f = new JFrame(PROGRAM_NAME);
+		Panel p = new Panel(f);
+		
+		p.init();
+		
+		f.add(p);
+		f.setSize(1280,720);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+		p.render();
+	}
+}
