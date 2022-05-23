6274ddb5033a8c18229bc2bbd11ac54bf7f0a7c0
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-23 17:17:21 +0000

    Update scripts Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,9 +3,15 @@
 COMMIT_MESSAGE="$*"
 while IFS= read -r line
 do
+if [ "$FIRST_LINE" = true ]; then
   COMMIT_MESSAGE+="
 
 Co-authored-by: $line"
+FIRST_LINE=false
+else
+  COMMIT_MESSAGE+="
+Co-authored-by: $line"
+fi
 done < utils/.coauthors
 git add -u
 git add *
